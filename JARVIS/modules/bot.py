2212ca3022db7a044324f024e247df9916ba5397
Finisherop <160778489+Finisherop@users.noreply.github.com>
2024-03-03 02:10:36 +0530

    Update bot.py

--- JARVIS/modules/bot.py
+++ JARVIS/modules/bot.py
@@ -99,7 +99,7 @@ async def addsudo(event):
         Heroku = heroku3.from_key(HEROKU_API_KEY)
         sudousers = getenv("SUDO_USERS", default=None)
 
-        ok = await event.reply(f"» __Jarvis Ka Ek Beta Aur Add Ho rha hai..__")
+        ok = await event.reply(f"» __🇳𝐎𝐓𝐓𝐘 Ka Ek Beta Aur Add Ho rha hai..__")
         target = ""
         if HEROKU_APP_NAME is not None:
             app = Heroku.app(HEROKU_APP_NAME)
@@ -127,7 +127,7 @@ async def addsudo(event):
             heroku_var["SUDO_USERS"] = newsudo    
     
     elif event.sender_id in SUDO_USERS:
-        await event.reply("» BSDK SIRF JARVIS SUDO DE SKTA HAI...")
+        await event.reply("» BSDK SIRF 𝐍𝐎𝐓𝐓𝐘 SUDO DE SKTA HAI...")
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sremovesudo(?: |$)(.*)" % hl))
 @X2.on(events.NewMessage(incoming=True, pattern=r"\%sremovesudo(?: |$)(.*)" % hl))
@@ -143,7 +143,7 @@ async def removesudo(event):
     if event.sender_id == OWNER_ID:
         Heroku = heroku3.from_key(HEROKU_API_KEY)
         sudousers = getenv("SUDO_USERS", default=None)
-        ok = await event.reply(f"YE Jarvis Ki Najayaz Aulad thi isiliye nikal diya💋...")
+        ok = await event.reply(f"YE 𝐍𝐎𝐓𝐓𝐘 Ki Najayaz Aulad thi isiliye nikal diya💋...")
         target = ""
         if HEROKU_APP_NAME is not None:
             app = Heroku.app(HEROKU_APP_NAME)
@@ -180,7 +180,7 @@ async def removesudo(event):
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%ssudos(?: |$)(.*)" % hl))
 async def show_sudo_users(event):
     if event.sender_id == OWNER_ID:
-        sudo_users_list = "Jarvis Ke Bache hai ye:\n"
+        sudo_users_list = "𝐍𝐎𝐓𝐓𝐘 Ke Bache hai ye:\n"
         for user_id in SUDO_USERS:
             sudo_users_list += f"- {user_id}\n"
         await event.reply(sudo_users_list)
