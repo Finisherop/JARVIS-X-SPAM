e7beba45eecc8a24e0de05371c2d1112f393f34b
Finisherop <160778489+Finisherop@users.noreply.github.com>
2024-03-08 14:39:43 +0530

    Update start.py

--- JARVIS/modules/start.py
+++ JARVIS/modules/start.py
@@ -5,14 +5,14 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_BUTTON = [
     [
-        Button.inline("• 🌸💌𝐂𝐎𝐌𝐌𝐀𝐍𝐃💌🌸 •", data="help_back")
+        Button.inline("• 💌𝐂𝐎𝐌𝐌𝐀𝐍𝐃💌 •", data="help_back")
     ],
     [
-        Button.url("• 🌸💌𝐂𝐇𝐀𝐍𝐍𝐄𝐋💌🌸 •", "https://t.me/NEXT_TOPPPER"),
-        Button.url("• 🌸💌𝐒𝐔𝐏𝐏𝐎𝐑𝐓💌🌸 •", "https://t.me/NEXT_TOPPPER")
+        Button.url("• 💌𝐂𝐡𝐚𝐧𝐧𝐞𝐥💌 •", "https://t.me/NEXT_TOPPPER"),
+        Button.url("• 💌𝐒𝐮𝐩𝐩𝐨𝐫𝐭💌 •", "https://t.me/NEXT_TOPPPER")
     ],
     [
-        Button.url("• 🌸💌𝐑𝐄𝐏𝐎💌🌸 •", "https://github.com/Finisherop/JARVIS-X-SPAM")
+        Button.url("• 💌𝐑𝐄𝐏𝐎💌 •", "https://github.com/Finisherop/JARVIS-X-SPAM")
     ]
 ]
 
@@ -35,9 +35,9 @@ async def start(event):
         bot_id = ANNIE.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
         TEXT += f"» **🇲𝐘 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑 : [𝐍𝐎𝐓𝐓𝐘 𝐁𝐎𝐘](https://t.me/II_ABOUT_FINISHER_II)**\n\n"
-        TEXT += f"» **𝐍𝐎𝐓𝐓𝐘𝐁𝐎𝐘 V2 :** `M 1.8.31`\n"
-        TEXT += f"»» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        TEXT += f"»» **𝐍𝐎𝐓𝐓𝐘𝐁𝐎𝐘 𝕍2 :** `M 1.8.31`\n"
+        TEXT += f"»» ** 𝑷𝒚𝒕𝒉𝒐𝒏 :** `3.11.3`\n"
+        TEXT += f"» ** 𝑻𝒆𝒍𝒆𝒈𝒓𝒂𝒎 𝒗𝒆𝒓𝒔𝒊𝒐𝒏 :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
               event.chat_id,
                     "https://telegra.ph/file/0bff810c9cade084f4848.jpg",
